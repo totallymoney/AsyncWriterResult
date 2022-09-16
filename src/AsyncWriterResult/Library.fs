135c6bb62817d53bbfd55bff5f2b3ad24b5b62bf
Nick Blair <blair.nicholas@gmail.com>
2022-09-16 13:22:29 +0100

    Add task writer result

--- src/AsyncWriterResult/Library.fs
+++ src/AsyncWriterResult/Library.fs
@@ -1,6 +1,8 @@
 ﻿[<AutoOpen>]
 module AsyncWriterResult
 
+open System.Threading.Tasks
+
 module Async =
 
     let retn x = async { return x }
@@ -24,10 +26,29 @@ module Async =
             return unwrappedF x
         }
 
-    let toTask f a = Async.StartAsTask(f a)
 
-    let runTaskSynchronously a =
-        a |> (Async.AwaitTask >> Async.RunSynchronously)
+module Task =
+
+    let retn x = task { return x }
+
+    let map f m =
+        task {
+            let! x = m
+            return f x
+        }
+
+    let bind f m =
+        task {
+            let! x = m
+            return! f x
+        }
+
+    let apply f m =
+        task {
+            let! unwrappedF = f
+            let! x = m
+            return unwrappedF x
+        }
 
 
 module Result =
@@ -219,9 +240,83 @@ module AsyncWriterResult =
         |> Async.map (List.ofArray >> WriterResult.collect)
 
 
+type TaskWriterResult<'ok, 'error, 'log> = Task<Writer<'log list, Result<'ok, 'error>>>
+
+
+[<AutoOpen>]
+module TaskWriterResult =
+
+    let retn x = x |> WriterResult.retn |> Task.retn
+
+    let map f = f |> WriterResult.map |> Task.map
+
+    let bind f m =
+        task {
+            let! w = m
+            let (r, logs1) = Writer.run w
+
+            match r with
+            | Ok a ->
+                let! ww = f a
+                let (b, logs2) = Writer.run ww
+                return Writer <| fun () -> b, logs1 @ logs2
+            | Error e -> return Writer <| fun () -> Error e, logs1
+        }
+
+    let apply f m =
+        task {
+            let! uf = f
+            let! um = m
+            let (r1, logs1) = Writer.run uf
+            let (r2, logs2) = Writer.run um
+
+            match r1, r2 with
+            | Ok g, Ok h -> return Writer <| fun () -> Ok(g h), logs1 @ logs2
+            | Error e1, _ -> return Writer <| fun () -> Error e1, logs1 @ logs2
+            | _, Error e2 -> return Writer <| fun () -> Error e2, logs1 @ logs2
+        }
+
+    module Operators =
+
+        let (<!>) = map
+        let (>>=) = bind
+        let (<*>) = apply
+
+    let write log =
+        task { return Writer(fun () -> Result.retn (), [ log ]) }
+
+    let mapError e m =
+        task {
+            let! w = m
+            let (r, logs) = Writer.run w
+            return Writer <| fun () -> Result.mapError e r, logs
+        }
+
+    let traverseResultM f list =
+
+        let (>>=) x f = bind f x
+
+        let cons head tail = head :: tail
+
+        let folder head tail =
+            f head
+            >>= (fun h -> tail >>= (fun t -> retn (cons h t)))
+
+        List.foldBack folder list (retn [])
+
+    let collect (tasks: TaskWriterResult<_, _, _> seq) =
+        Task.WhenAll tasks
+        |> Task.map (List.ofArray >> WriterResult.collect)
+
+
 module AsyncWriter =
 
     let retn a = Writer.retn a |> Async.retn
+
+
+module TaskWriter =
+
+    let retn a = Writer.retn a |> Task.retn
 
 
 
@@ -259,3 +354,12 @@ type AsyncWriterResultBuilder() =
     member __.Zero() = __.Return()
 
 let asyncWriterResult = AsyncWriterResultBuilder()
+
+
+type TaskWriterResultBuilder() =
+    member __.Return(x) = TaskWriterResult.retn x
+    member __.ReturnFrom(m: Task<Writer<'w, Result<'a, 'b>>>) = m
+    member __.Bind(m, f) = TaskWriterResult.bind f m
+    member __.Zero() = __.Return()
+
+let taskWriterResult = TaskWriterResultBuilder()

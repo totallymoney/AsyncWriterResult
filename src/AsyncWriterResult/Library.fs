424a39be387b16d6cc0f4997754de80c50632b9a
Michael Winch <michaeljwinch@gmail.com>
2024-04-23 14:59:22 +0100

    Make  execute in parallel

--- src/AsyncWriterResult/Library.fs
+++ src/AsyncWriterResult/Library.fs
@@ -27,8 +27,15 @@ module Async =
             return unwrappedF x
         }
 
-    let zip left right =
-        bind (fun l -> bind (fun r -> retn (l, r)) right) left
+    let zip c1 c2 =
+        async {
+            let! ct = Async.CancellationToken
+            let x = Async.StartImmediateAsTask (c1, cancellationToken=ct)
+            let y = Async.StartImmediateAsTask (c2, cancellationToken=ct)
+            let! x' = Async.AwaitTask x
+            let! y' = Async.AwaitTask y
+            return x', y'
+        }
 
 
 module Result =

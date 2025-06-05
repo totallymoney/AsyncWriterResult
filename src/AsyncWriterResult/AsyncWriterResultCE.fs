5e62b7ea3ccb467d411b95d53b043b7ac4a5bd14
edgargonzalez <egonzalez@totallymoney.com>
2025-06-05 10:56:35 +0100

    update FSToolkit to v5

--- src/AsyncWriterResult/AsyncWriterResultCE.fs
+++ src/AsyncWriterResult/AsyncWriterResultCE.fs
@@ -67,14 +67,14 @@ module AsyncWriterResultCE =
     module AsyncWriterResultBuilderExtensions =
         type AsyncWriterResultBuilder with
             member __.Source(x: Result<'a, 'b>) = x |> AsyncWriter.retn
-            member __.Source(x: Writer<'w, 't>) = x |> Writer.map Ok |> Async.retn
+            member __.Source(x: Writer<'w, 't>) = x |> Writer.map Ok |> Async.singleton
             member __.Source(x: Async<'t>) = x |> Async.map WriterResult.retn
             member __.Source(x: Task<'t>) = x |> Async.AwaitTask |> Async.map WriterResult.retn
 
     [<AutoOpen>]
     module AsyncWriterResultBuilderExtensionsHighPriority =
         type AsyncWriterResultBuilder with
-            member __.Source(x: Writer<'w, Result<'a, 'b>>) = x |> Async.retn
+            member __.Source(x: Writer<'w, Result<'a, 'b>>) = x |> Async.singleton
             member __.Source(x: Async<Result<'a, 'b>>) = x |> Async.map Writer.retn
             member __.Source(x: Async<Writer<'w, 't>>) = x |> AsyncWriter.map Result.retn
             member __.Source(x: Task<Result<'a, 'b>>) = x |> Async.AwaitTask |> Async.map Writer.retn

fb8adcbb05fbe9a650349fa51ad1d6203fa4d766
Edgar Gonzalez <egonzalez@totallymoney.com>
2025-06-06 09:41:20 +0100

    Update FSToolkit to v5 (#10)
    * update FSToolkit to v5
    
    * update to net8 and paket 8
    
    * update workflows to net8

--- src/AsyncWriterResult/AsyncWriter.fs
+++ src/AsyncWriterResult/AsyncWriter.fs
@@ -5,7 +5,7 @@ open FsToolkit.ErrorHandling
 [<RequireQualifiedAccess>]
 module AsyncWriter =
 
-    let retn a = Writer.retn a |> Async.retn
+    let retn a = Writer.retn a |> Async.singleton
 
     let map f = f |> Writer.map |> Async.map
 
